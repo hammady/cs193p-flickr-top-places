8f58b650f358ba5605dea07dabbf726c687e5ac8
Hossam Hammady <github@hammady.net>
2012-02-22 23:24:08 +0300

    Zooming functionality, min, max and initial zoom scale, support for auto rotation

--- FlickrTopPlaces/FlickrImageViewController.m
+++ FlickrTopPlaces/FlickrImageViewController.m
@@ -9,10 +9,11 @@
 #import "FlickrImageViewController.h"
 #import "FlickrFetcherHelper.h"
 
-@interface FlickrImageViewController()
+@interface FlickrImageViewController()  <UIScrollViewDelegate>
 @property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-
+-(void) recalculateMinAndMaxZoomScale;
+-(void) displayWholeImage;
 @end
 
 @implementation FlickrImageViewController
@@ -28,13 +29,12 @@
 }
 */
 
-/*
 // Implement viewDidLoad to do additional setup after loading the view, typically from a nib.
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.scrollView.delegate = self;
 }
-*/
 
 - (void)viewDidUnload
 {
@@ -47,8 +47,43 @@
 
 - (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
 {
-    // Return YES for supported orientations
-    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+    return YES;
+}
+
+-(void) didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation
+{
+    [self recalculateMinAndMaxZoomScale];
+    //[self displayWholeImage];
+    // because min/max zoom scales have changed, we might be in a state where
+    // the zoom scale is lower than min or larger than max, fix this
+    if (self.scrollView.zoomScale < self.scrollView.minimumZoomScale)
+        self.scrollView.zoomScale = self.scrollView.minimumZoomScale;
+    else if (self.scrollView.zoomScale > self.scrollView.maximumZoomScale)
+        self.scrollView.zoomScale = self.scrollView.maximumZoomScale;
+}
+
+-(void) recalculateMinAndMaxZoomScale
+{
+    // calculate min and max zoom scale
+    CGFloat xPixels, yPixels, xMinZoom, yMinZoom, xMaxZoom, yMaxZoom;
+    // we multiply by contentScaleFactor to get pixels rather than points (retina)
+    xPixels = self.scrollView.bounds.size.width * self.scrollView.contentScaleFactor;
+    yPixels = self.scrollView.bounds.size.height * self.scrollView.contentScaleFactor;
+    xMinZoom = xPixels / self.imageView.image.size.width;
+    yMinZoom = yPixels / self.imageView.image.size.height;
+    xMaxZoom = 1 / xMinZoom;
+    yMaxZoom = 1 / yMinZoom;
+    self.scrollView.minimumZoomScale = MIN(xMinZoom, yMinZoom);
+    self.scrollView.maximumZoomScale = MAX(xMaxZoom, yMaxZoom);
+}
+
+-(void) displayWholeImage
+{
+    // we can display the whole image by either zoomin to minZoom
+    self.scrollView.zoomScale = self.scrollView.minimumZoomScale;
+    // or by zooming to the whole image rect
+    //[self.scrollView zoomToRect:self.imageView.frame animated:NO];
+    self.scrollView.contentOffset = CGPointZero;
 }
 
 -(void) reloadImageWithInfo:(NSDictionary *)info
@@ -69,7 +104,9 @@
         imageData = [FlickrFetcherHelper 
                              imageDataForPhotoWithFlickrInfo:info 
                              format:FlickrFetcherPhotoFormatLarge];
-        NSLog(@"Image data length returned: %x", imageData.length);
+        NSString* url = [FlickrFetcherHelper urlStringForPhotoWithFlickrInfo:info
+                                                      format:FlickrFetcherPhotoFormatLarge];
+        NSLog(@"Image url: %@ with data length: %x", url, imageData.length);
         
         dispatch_async(dispatch_get_main_queue(), ^{
             [spinner stopAnimating];
@@ -77,9 +114,19 @@
             self.imageView.image = image;
             self.imageView.frame = CGRectMake(0, 0, image.size.width, image.size.height);
             self.scrollView.contentSize = image.size;
+            
+            [self recalculateMinAndMaxZoomScale];
+            [self displayWholeImage];
         });
     });
     dispatch_release(photosQueue);    
+}
+
+#pragma makr UIScrollViewDelegate methods
+
+-(UIView*) viewForZoomingInScrollView:(UIScrollView *)scrollView
+{
+    return self.imageView;
 }
 
 @end
