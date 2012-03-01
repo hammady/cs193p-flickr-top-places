3668f5b1028ec0d354cb54da4feb2f399f258c66
Hossam Hammady <github@hammady.net>
2012-03-02 01:05:16 +0300

    Implemented cache size limit feature for cached photos

--- FlickrTopPlaces/FlickrImage.m
+++ FlickrTopPlaces/FlickrImage.m
@@ -7,6 +7,7 @@
 //
 
 #import "FlickrImage.h"
+#define CACHE_SUBDIR            @"images"
 #define CACHE_SUBDIR_SQUARE     @"square"
 #define CACHE_SUBDIR_LARGE      @"large"
 #define CACHE_SUBDIR_THUMBNAIL  @"thumbnail"
@@ -14,9 +15,65 @@
 #define CACHE_SUBDIR_MEDIUM     @"medium"
 #define CACHE_SUBDIR_ORIGINAL   @"original"
 #define CACHE_SUBDIR_UNDEFINED  @"undefined"
+#define CACHE_SIZE_MAX        (10*1024*1024)
+#define CACHE_SIZE_MIN        (7*1024*1024)
 
+@interface FlickrImage()
+
+@end
 
 @implementation FlickrImage
+
+static NSFileManager* _fileManager;
+static NSString* _cacheSubdir;
+static UInt64 _cacheSize;
+
++(NSFileManager*) fileManager
+{
+    if (_fileManager == nil)
+        _fileManager = [[NSFileManager alloc] init];
+    return _fileManager;
+}
+
++(NSString*) cacheSubdir
+{
+    if (_cacheSubdir == nil) 
+        _cacheSubdir = [[[[[self fileManager] URLsForDirectory:NSCachesDirectory 
+                                  inDomains:NSUserDomainMask]
+              lastObject] path] stringByAppendingPathComponent:CACHE_SUBDIR];
+    return _cacheSubdir;
+}
+
++(void) createCacheSubdir
+{
+    [[self fileManager] createDirectoryAtPath:[self cacheSubdir] 
+                  withIntermediateDirectories:NO attributes:nil error:nil];
+}
+
++(UInt64) sizeOfDirectoryAtPath:(NSString*) path
+{
+    // very expensive function, rarely call
+    NSFileManager *fileManager = [self fileManager];
+    NSArray *files = [fileManager subpathsOfDirectoryAtPath:path error:nil];
+    UInt64 totalSize = 0;
+    for (NSString *file in files) {
+        NSDictionary* attribs = [fileManager attributesOfItemAtPath:[path stringByAppendingPathComponent:file] error:nil];
+        totalSize += [[attribs objectForKey:NSFileSize] unsignedLongLongValue];
+    }
+    return totalSize;
+}
+
++(UInt64) cacheSize
+{
+    if (_cacheSize == 0)
+        _cacheSize = [self sizeOfDirectoryAtPath:[self cacheSubdir]];
+    return _cacheSize;
+}
+
++(void) setCachSize:(UInt64) size
+{
+    _cacheSize = size;
+}
 
 +(NSString*) cacheSuffixForFormat:(FlickrFetcherPhotoFormat) format
 {
@@ -40,14 +97,10 @@
 
 +(id) cachedImageData:(NSDictionary*) info format:(FlickrFetcherPhotoFormat) format
 {
-    NSFileManager* fileManager = [[NSFileManager alloc] init];
-    NSString* imageCacheDir = [[[fileManager URLsForDirectory:NSCachesDirectory 
-                                                   inDomains:NSUserDomainMask]
-                               lastObject] path];
-    NSString* imageFilePath = [[imageCacheDir stringByAppendingPathComponent:
+    NSString* imageFilePath = [[[self cacheSubdir] stringByAppendingPathComponent:
                                [info objectForKey:PHOTO_DICT_ID]] 
                                stringByAppendingString:[self cacheSuffixForFormat:format]];
-    BOOL cached = [fileManager isReadableFileAtPath:imageFilePath];
+    BOOL cached = [[self fileManager] isReadableFileAtPath:imageFilePath];
     
     if (cached) {
         NSData* imageData = [[NSData alloc] initWithContentsOfFile:imageFilePath];
@@ -59,7 +112,47 @@
 
 +(void) cacheImageWithData:(NSData*) data filePath:(NSString*) path
 {
+    [self createCacheSubdir];
+
     [data writeToFile:path atomically:YES];
+
+    NSFileManager *fileManager = [self fileManager];
+    NSString* cacheDir = [self cacheSubdir];
+    UInt64 cacheSize = [self cacheSize];
+    
+    NSDictionary* attribs = [fileManager attributesOfItemAtPath:path error:nil];
+    UInt64 newFilesize = [[attribs objectForKey:NSFileSize] unsignedLongLongValue];
+    cacheSize += newFilesize;
+    [self setCachSize: cacheSize];
+    
+    // limit cache size
+    if (cacheSize > CACHE_SIZE_MAX) {
+        NSMutableArray *files = [[fileManager subpathsOfDirectoryAtPath:cacheDir error:nil] mutableCopy];
+        // sort from newest to oldest
+        [files sortUsingComparator:^NSComparisonResult(id obj1, id obj2) {
+            NSString* file1 = [cacheDir stringByAppendingPathComponent:obj1];
+            NSString* file2 = [cacheDir stringByAppendingPathComponent:obj2];
+            NSDictionary* attribs1 = [fileManager attributesOfItemAtPath:file1 error:nil];
+            NSDictionary* attribs2 = [fileManager attributesOfItemAtPath:file2 error:nil];
+            NSDate* date1 = [(NSDictionary*) attribs1 objectForKey:NSFileCreationDate];
+            NSDate* date2 = [(NSDictionary*) attribs2 objectForKey:NSFileCreationDate];
+            return [date1 compare:date2];  
+        }];
+        UInt64 filesize;
+        for (NSString *file in files) {
+            NSString* filename = [cacheDir stringByAppendingPathComponent:file];
+            NSDictionary* attribs = [fileManager attributesOfItemAtPath:filename error:nil];
+            filesize = [[attribs objectForKey:NSFileSize] unsignedLongLongValue];
+            [fileManager removeItemAtPath:filename error:nil];
+            cacheSize -= filesize;
+            if (cacheSize <= CACHE_SIZE_MIN)
+                break;
+            // continue evacuating till min is reached so that there is room
+            // for several images to come in again and not trigger this 
+            // expensive function on each cached image
+        }
+        [self setCachSize:cacheSize];
+    }
 }
 
 +(UIImage*) imageWithInfo:(NSDictionary *)info format:(FlickrFetcherPhotoFormat)format
