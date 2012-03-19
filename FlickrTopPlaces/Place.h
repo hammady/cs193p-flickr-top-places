3a2ec386d6902f71e34ca3c5e92cd2a8245631f8
Hossam Hammady <github@hammady.net>
2012-03-19 09:38:32 +0300

    committing more files

---
+++ FlickrTopPlaces/Place.h
@@ -0,0 +1,31 @@
+//
+//  Place.h
+//  FlickrTopPlaces
+//
+//  Created by Hossam Hammady on 3/16/12.
+//  Copyright (c) 2012 Texas A&M University at Qatar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <CoreData/CoreData.h>
+
+@class Photo;
+
+@interface Place : NSManagedObject
+
+@property (nonatomic, retain) NSDate * firstVisitedAt;
+@property (nonatomic, retain) NSNumber * latitude;
+@property (nonatomic, retain) NSNumber * longitude;
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * unique;
+@property (nonatomic, retain) NSSet *photos;
+@end
+
+@interface Place (CoreDataGeneratedAccessors)
+
+- (void)addPhotosObject:(Photo *)value;
+- (void)removePhotosObject:(Photo *)value;
+- (void)addPhotos:(NSSet *)values;
+- (void)removePhotos:(NSSet *)values;
+
+@end
---
+++ FlickrTopPlaces/Place.m
@@ -0,0 +1,22 @@
+//
+//  Place.m
+//  FlickrTopPlaces
+//
+//  Created by Hossam Hammady on 3/16/12.
+//  Copyright (c) 2012 Texas A&M University at Qatar. All rights reserved.
+//
+
+#import "Place.h"
+#import "Photo.h"
+
+
+@implementation Place
+
+@dynamic firstVisitedAt;
+@dynamic latitude;
+@dynamic longitude;
+@dynamic name;
+@dynamic unique;
+@dynamic photos;
+
+@end
